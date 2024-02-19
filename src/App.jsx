ae9719de5670c4cc8457fd3380db39671256ffdf
Trevis Liu <trevisliu@gmail.com>
2024-02-18 20:27:31 -0500

    Add layout and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home/Home.jsx';
-import News from './pages/News';
-import Sport from './pages/Sport.jsx'
-import Radio from './pages/Radio.jsx'
-import TV from './pages/TV.jsx'
-import Trending from "./pages/Trending.jsx";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Article from "./pages/Article/Article.jsx";
 
 const App = () => {
-   return (
-      <Router>
-         <div>
-            <Switch>
-               <Route path="/" exact component={Home} />
-               <Route path="/news" component={News} />
-               <Route path="/sport" component={Sport} />
-               <Route path="/radio" component={Radio} />
-               <Route path="/tv" component={TV} />
-               <Route path="/trending" component={Trending} />
-            </Switch>
-         </div>
-      </Router>
-   );
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/article" component={Article} />
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,45 +1,45 @@
 import React from 'react';
-import NavBar from '../../components/NavBar/NavBar.jsx';
 import './Home.css';
 
 const Home = () => {
    return (
       <div>
-         <NavBar/>
-
          <div className="home">
             <section className="top-section">
-               <h1>Welcome to SYT News</h1>
+               <a href="/">
+                  <img src="/src/images/logo.png" alt="SYT News Logo" width={150} height={50}/>
+               </a>
+               <h1>SYT News</h1>
                <p>Stay updated with the latest news and updates</p>
             </section>
             <section className="latest-news-section">
                <h2>Latest News</h2>
                <div className="news-item">
-                  <h3>News Title 1</h3>
-                  <p>Description of the news item goes here...</p>
+                  <h3>What next for Putin? After Navalny's death, many fear what leader will move on to</h3>
+                  <p>With Ukraine retreating and western sanctions having little impact, the Russian president is growing bolder and may embark on more reckless moves</p>
                </div>
                <div className="news-item">
-                  <h3>News Title 2</h3>
-                  <p>Description of the news item goes here...</p>
+                  <h3>What you should be doing for your 2023 tax return right now</h3>
+                  <p>Tax expert Jamie Golombek offers tips on CRA deadlines and changes</p>
                </div>
                <div className="news-item">
-                  <h3>News Title 3</h3>
-                  <p>Description of the news item goes here...</p>
+                  <h3>TTC service changes starting Sunday</h3>
+                  <p>Several changes to TTC service beginning this Sunday</p>
                </div>
             </section>
             <section className="featured-section">
                <h2>Sport Articles</h2>
                <div className="article">
                   <h3>World cup</h3>
-                  <p>World Cup article goes here...</p>
+                  <p>Canada Prepares for Soccer's 2026 World Cup</p>
                </div>
                <div className="article">
                   <h3>Premier League</h3>
-                  <p>Premier League article goes here...</p>
+                  <p>Hojlund makes history as Man Utd close gap to Spurs</p>
                </div>
                <div className="article">
                   <h3>NBA</h3>
-                  <p>NBA article goes here...</p>
+                  <p>Scottie Barnes' journey to NBA All-Star</p>
                </div>
             </section>
          </div>
