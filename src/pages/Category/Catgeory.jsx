e9ae1a3bc0c87d81836519fa8a89349c420ebf9f
Trevis Liu <trevisliu@gmail.com>
2024-04-18 23:12:16 -0400

    Add icons to category page

--- src/pages/Category/Catgeory.jsx
+++ src/pages/Category/Catgeory.jsx
@@ -2,9 +2,16 @@ import React, { useEffect, useState } from "react";
 
 import "./Category.css";
 import { Link } from "react-router-dom";
+import { FaGlobeAmericas, FaMapMarkerAlt } from "react-icons/fa";
+import { IoFootball } from "react-icons/io5";
 
 const Category = ({ categoryName, articles }) => {
   const [data, setData] = useState();
+  const categoryIcons = {
+    local: <FaMapMarkerAlt className="mb-1" />,
+    global: <FaGlobeAmericas className="mb-1" />,
+    sports: <IoFootball className="mb-1" />,
+  };
 
   useEffect(() => {
     (async () => {
@@ -31,12 +38,14 @@ const Category = ({ categoryName, articles }) => {
         className="display-1 mt-5 text-center"
         style={{ fontVariant: "small-caps" }}
       >
+        {categoryIcons[categoryName]}
+        &nbsp;
         {categoryName.charAt(0).toUpperCase() + categoryName.slice(1)}
       </h1>
       <hr className="mb-5" />
       {data &&
         data.map((entry, idx) => (
-          <Link to={`/article/${entry.path}`}>
+          <Link key={idx} to={`/article/${entry.path}`}>
             <div className="row item p-3">
               <div className="col-md-3 my-1">
                 <img className="img-fluid rounded" src={entry.image} />
